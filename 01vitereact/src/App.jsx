c81ef4944cbc2e79e6bb24bc691275fe3df9c1f2
Disha Garg <142096226+Disha-Garg04@users.noreply.github.com>
2024-12-28 14:40:46 +0530

    Initial commit

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+import First from "./first"
+
+function App() {
+
+  return (
+    <>
+      <First />
+      <h1>chai aur react</h1>
+      <p>HII Disha Here</p>
+    </>
+    
+  )
+}
+
+export default App
---
+++ 01vitereact/src/first.jsx
@@ -0,0 +1,6 @@
+function First(){
+    return(
+        <h3>My first react project</h3>
+    )
+}
+export default First
