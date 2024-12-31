07913ada39b34e99e68b63d11f913ae472378608
Disha Garg <142096226+Disha-Garg04@users.noreply.github.com>
2024-12-31 23:10:55 +0530

    UPDATED

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,8 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp() {
+    return (
+        <>
+    <h1>Hii i m function</h1>
+    <p>hii </p>
+    </>
+    )
+}
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <MyApp />
 )
